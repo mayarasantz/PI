1df3579073bcead7ec069456f517b3ee1e8e45fe
Mayara Timoteo <mayara.t@aluno.ifsp.edu.br>
2023-09-18 09:10:22 -0300

    inserir dados

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express();
+app.use(express.urlencoded({extended: true}));
 app.use(express.json())
-app.listen (9000, () => console. 
-log("ok"));
+app.listen (9000, () => console. log("ok"));
 
 const mysql = require('mysql2/promise')
 const connection = mysql.createPool({
@@ -14,13 +14,11 @@ const connection = mysql.createPool({
 
 app.get('/',(req,res)=>{
     res.send("Mayara e Inae")
-    
-
 })
 
 const getAllpessoas = async () => {
     const [query] = await connection
-    .execute('select * from testepessoa.pessoa');
+    .execute('select * from testepessoa.pessoas');
     return query;
     }
     
@@ -28,3 +26,18 @@ app.get('/pessoa', async (req,res) => {
     const consulta = await getAllpessoas();
     return res.status(200).json(consulta);
     })
+
+
+app.get('/pessoa/:id', async (req, res)=>{
+    const {id} = req.params;
+    const {query} = await connection.execute('select * from TestePessoa.Pessoa where id = ?' , {id});
+    if(query.length === 0) return res.status (400).json({mensagem: ' Nao encontrado.'})
+    return res.status(200).json(query);
+    })
+
+app.post('/pessoa', async (req,res)=>{
+    const {nome, email} = req.body;
+    const [query]= await connection.execute('insert testepessoa.pessoas (nome,email) values(?, ?)',
+    [nome,email])
+    return res.status(200).json(query);
+} )
